d217929aabe0407b21ec8dd10fbdd52b941751ec
bikramserv <bikram.mohanty007@gmail.com>
2025-05-11 16:04:25 +0200

    new3

---
+++ test_api_response.py
@@ -0,0 +1,31 @@
+from contextlib import nullcontext
+
+import requests
+import pytest
+
+#Base url of the API
+
+API_URL = "https://app-as-webhook-test.azurewebsites.net/"
+
+
+
+def test_get_resp():
+
+ res = requests.get(API_URL)
+ assert res.status_code == 200
+
+ json_data = res.json()
+
+ # print (json_data)
+ if json_data and json_data.get("event") == "verification.complete":
+     print("verification complete event is found")
+
+     for verification_id in json_data ["data"]["id"]:
+         print (f"The id of completed verifications are : {verification_id}")
+
+     #print(json_data)
+ else:
+     print("No tests executed")
+
+ # Validate the key and value check partial
+
